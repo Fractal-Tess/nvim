27db38ac94210a1c46ea32cbd065a476f8315d57
Fractal-Tess <vgfractal@gmail.com>
2024-09-21 20:21:00 +0300

    refactor lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,52 +1,40 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-
-local lspconfig = require "lspconfig"
+local configs = require "nvchad.configs.lspconfig"
 
 local servers = {
-  "biome",
-  "html", -- html
-  "cssls", -- css, scss, sass, less
-  "ts_ls", -- js, ts, jsx, tsx
-  "clangd", -- c, cpp
-  "svelte", -- svelte
-  "astro", -- astro
-  "tailwindcss", -- tailwindcss
-  "rust_analyzer", -- rust
-  "eslint", -- js, ts, jsx, tsx (eslint)
-  "emmet_language_server", -- emmet
-  "nil_ls", -- lua
-  "csharp_ls", -- dotnet sdk
-  "jsonls", -- json
-  "sqls", -- sql
-  -- "phpactor", -- php,
-  "phan", -- php
-}
-
-for _, lsp in ipairs(servers) do
-  if lsp == "tailwindcss" then
-    lspconfig[lsp].setup {
-      on_attach = on_attach,
-      on_init = on_init,
-      capabilities = capabilities,
-      settings = {
-        tailwindCSS = {
-          experimental = {
-            classRegex = {
-              { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
-              { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
-              { "cn\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
-            },
+  biome = {},
+  html = {},
+  cssls = {},
+  -- ts_ls = {},
+  clangd = {},
+  svelte = {},
+  astro = {},
+  tailwindcss = {
+    settings = {
+      tailwindCSS = {
+        experimental = {
+          classRegex = {
+            { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+            { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+            { "cn\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
           },
         },
       },
-    }
-  else
-    lspconfig[lsp].setup {
-      on_init = on_init,
-      on_attach = on_attach,
-      capabilities = capabilities,
-    }
-  end
+    },
+  },
+  rust_analyzer = {},
+  eslint = {},
+  emmet_language_server = {},
+  lua_ls = {},
+  csharp_ls = {},
+  jsonls = {},
+  nginx_language_server = {},
+  sqls = {},
+  phan = {},
+}
+
+for name, opts in pairs(servers) do
+  opts.on_init = configs.on_init
+  opts.on_attach = configs.on_attach
+  opts.capabilities = configs.capabilities
+  require("lspconfig")[name].setup(opts)
 end
